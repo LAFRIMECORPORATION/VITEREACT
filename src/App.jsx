96bdc4ba60ea5707ca751c7830b8270a99a9c5cc
lafrimecorporation <merimesiewe84@gmail.com>
2026-01-25 11:29:55 +0100

    'upate'

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,7 @@ const MainRoutes = ({ isAuthenticated, setIsAuthenticated, tokenExpired }) => {
       <nav className="nav">
         <Link to="/" className={location.pathname === "/" ? "active-link" : ""}>Accueil <FaHome /> </Link>
         <Link to="/a-propos" className={location.pathname === "/a-propos" ? "active-link" : ""}>A Propos <FaInfoCircle /></Link>
-        <Link to="/Publier" className={location.pathname === "/Publier" ? "active-link" : ""}><span> inover</span><FaPlusCircle /> </Link>
+        <Link to="/Publier" className={location.pathname === "/Publier" ? "active-link" : ""}><span> inovation</span><FaPlusCircle /> </Link>
         <Link to="/mon-compte" className={location.pathname === "/mon-compte" ? "active-link" : ""}>Mon Compte <FaUser /></Link>
         <Link to="/notification" className={location.pathname === "/notification" ? "active-link" : ""}>Notification <FaUsers /></Link>
       </nav>
